6233436fcf61cbb27186b336796071e3be9b0d19
ArnoldQiu <arrnoldc@hotmail.com>
2017-03-20 17:03:38 +0800

    change

--- app/src/main/java/arnoldc/githubtest2/MainActivity2.java
+++ app/src/main/java/arnoldc/githubtest2/MainActivity2.java
@@ -1,25 +1,34 @@
 package arnoldc.githubtest2;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.view.Menu;
+import android.view.MenuItem;
 import android.content.Intent;
-import android.view.View;
-import android.widget.Button;
+
 
 public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.setContentView(R.layout.activity_main2);
-        final Button finishSecondActivity = (Button) super.findViewById(R.id.back_button);
-        finishSecondActivity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i1 = new Intent();
-                i1.setClass(MainActivity2.this,MainActivity.class);
-                startActivity(i1);
-            }
-        });
+        setContentView(R.layout.activity_main2);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main2,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem action_next) {
+        int id= action_next.getItemId();
+        if (id == R.id.action_next){
+            Intent i1 = new Intent();
+            i1.setClass(MainActivity2.this,MainActivity.class);
+            startActivity(i1);
+        }
+        return  super.onOptionsItemSelected(action_next);
     }
 }
